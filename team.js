ffaac5e6e43e596fe54f33229b0ca308be90bddb
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-05-19 23:57:40 -0400

    team 2

--- team.js
+++ team.js
@@ -2,6 +2,9 @@
 // Problem Link - https://codeforces.com/problemset/problem/231/A
 
 let n = 3;
-let a = [1, 1, 0];
-let b = [1, 1, 1];
-let c = [1, 0, 0];
+let arrays = [
+  [1, 1, 0],
+  [1, 1, 1],
+  [1, 0, 0],
+];
+let salida = 0;
